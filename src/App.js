bf06767864bf59a9661b3e22bc9f5f9079b2d6bf
KhanSelf <bloodelfate@naver.com>
2019-08-22 22:58:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return (class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
+}
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
